52577563cd09d40a481b0bf39a8ad1f4677ecca2
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-08-03 21:06:42 -0600

    Logical operators: And, or, not.

--- Ch01/01_01/01_01.py
+++ Ch01/01_01/01_01.py
@@ -33,6 +33,6 @@ if not isRaining:
 
 ages = [12, 18, 39, 87, 7, 2]    
 for age in ages:
-    isAdult = age > 17;
+    isAdult = age > 17
     if not isAdult:
         print("Being " + str(age) + " does not make you an adult.")
